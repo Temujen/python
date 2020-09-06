3408d9dccdec0c575fea6d7e0e375372394b2276
Temujen <talalayartem@mail.ru>
2020-09-06 22:26:26 +0300

    fixed task_1

--- Lesson_1/task_1.py
+++ Lesson_1/task_1.py
@@ -2,7 +2,7 @@ name = input("Enter your name: ")
 age = int(input(f"Hello {name}, how are you old?: "))
 if age < 18:
     print("You are young person")
-elif age >= 18 <= 40:
+elif age >= 18 and age <= 40:
     print("You are middle-aged person")
 elif age > 40:
     print("You are old person")
