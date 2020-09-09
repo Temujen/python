add98605d253d7c2b6ec736ccf2ea27d2e45747f
Temujen <talalayartem@mail.ru>
2020-09-09 23:35:09 +0300

    update task_3

--- Lesson_2/task_3.py
+++ Lesson_2/task_3.py
@@ -2,13 +2,12 @@ num = int(input("Enter month number: "))
 while num <= 0 or num > 12:
     print("Your enter number <= 0, please try again")
     num = int(input("Enter month number: "))
-season = [
-    {12: "Winter"}, {1: "Winter"}, {2: "Winter"},
-    {3: "Spring"}, {4: "Spring"}, {5: "Spring"},
-    {6: "Summer"}, {7: "Summer"}, {8: "Summer"},
-    {9: "Autumn"}, {10: "Autumn"}, {11: "Autumn"}
-]
-for i in range(len(season)):
-    if num in season[i].keys():
-        test = str(season[i].get(num))
-        print(f"This is {test}")
+season = {
+    "Winter": [12, 1, 2],
+    "Spring": [3, 4, 5],
+    "Summer": [6, 7, 8],
+    "Autumn": [9, 10, 11]
+}
+for key in season.keys():
+    if num in season[key]:
+        print(f"This is {key}")
