f9b8ec1c9b28217bb1a86649c7e8d006a4247536
Temujen <talalayartem@mail.ru>
2020-09-09 20:29:51 +0300

    update task_1

--- Lesson_2/task_1.py
+++ Lesson_2/task_1.py
@@ -1,3 +1,3 @@
-source = [5, (1, 2), ('a', 'b'), {1: "key", 2: "key2"}, True, None,]
+source = [5, "my_string", (1, 2), {1: "key", 2: "key2"}, True, None]
 for i in range(len(source)):
     print(type(source[i]))
