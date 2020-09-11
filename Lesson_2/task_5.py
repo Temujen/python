96fac141a8659fcb95cc99d0411fe1aecbea7396
Temujen <talalayartem@mail.ru>
2020-09-11 11:08:37 +0300

    update task_5

--- Lesson_2/task_5.py
+++ Lesson_2/task_5.py
@@ -2,12 +2,12 @@ my_list = [7, 5, 3, 3, 2]
 number = input("Enter mark or enter 'q' for exit: ")
 while number != "q":
     i = 0
-    number = int(number)
-    while i <= len(my_list):
-        if number >= my_list[i]:
+    number = float(number)
+    while i < len(my_list):
+        if number > my_list[i]:
             my_list.insert(i, number)
             i = len(my_list)
-        elif number < my_list[len(my_list) - 1]:
+        elif number <= my_list[len(my_list) - 1]:
             my_list.append(number)
             i = len(my_list)
         i += 1
