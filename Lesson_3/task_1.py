b87c7a143658fbd32d548187a8f71a66b598c313
Temujen <talalayartem@mail.ru>
2020-09-14 21:47:32 +0300

    update task_1

--- Lesson_3/task_1.py
+++ Lesson_3/task_1.py
@@ -1,11 +1,11 @@
 def div_function(var1, var2):
     try:
-        result = var1 / var2
+        result = float(var1) / float(var2)
         return round(result, 2)
-    except ZeroDivisionError as err:
-        print(f"{err}, Please try again")
+    except (ZeroDivisionError, ValueError) as err:
+        print(f"{err}\n Please try again")
 
 
-number1 = int(input("Enter first number: "))
-number2 = int(input("Enter second number: "))
+number1 = input("Enter first number: ")
+number2 = input("Enter second number: ")
 print(div_function(number1, number2))
